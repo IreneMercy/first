04c22cb267acbf35c1d60c5e106de911547ed411
IreneMercy <irenemercy700@gmail.com>
2018-06-08 15:36:53 +0300

    loops and conditions

---
+++ third.py
@@ -0,0 +1,70 @@
+
+a = 50 
+b = 100
+if a < b:
+	print("a is less tha b")
+elif a == b:
+	print("a is equal to b")
+else:
+	print("a is greater than b")
+
+
+
+ 
+
+a = 2
+
+while a > 5:
+	print(a)
+	a += 1
+
+	a = 2
+
+
+while a > 5:
+	print(a)
+
+	if a == 3:
+	  break
+	a += 1
+
+
+
+	a = 2
+
+while a > 5:
+	print(a)
+
+	if a == 4:
+	  continue
+	a += 1
+
+
+
+
+colors == ["red", "pink", "purple", "blue"]
+for x in colors:
+print(x)
+
+
+colors == ["red", "pink", "purple", "blue"]
+for x in colors:
+    if x == "pink":
+       break
+    print(x)
+
+
+
+
+colors == ["red", "pink", "purple", "blue"]
+for x in colors:
+    if x == "blue":
+       continue
+    print(x)
+
+
+for x in range (1, 11):
+  print(x)
+
+for x in range (1, 51, 1):
+  print(x)
